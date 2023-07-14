297cdfe0e39a3ed4d67f61a93c9bed9c850080be
Marcin <marcinzub1@gmail.com>
2023-07-14 14:11:58 +0200

    Initial commit

---
+++ src/main/java/pl/marcin/zubrzycki/rewards/CodingAssignmentApplication.java
@@ -0,0 +1,13 @@
+package pl.marcin.zubrzycki.rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingAssignmentApplication.class, args);
+	}
+
+}
